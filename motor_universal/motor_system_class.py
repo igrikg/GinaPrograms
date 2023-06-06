50b8dd89fe8e53baa4451221c7afb9a3b4707f08
Igor Gapon Work <igrikg@gmail.com>
2023-06-06 16:03:31 +0200

    update

--- motor_universal/motor_system_class.py
+++ motor_universal/motor_system_class.py
@@ -1,31 +1,38 @@
 from comportoperation import SerialDevice
 
+
 class MotorsSystems:
     def __init__(self, config: dict):
         self.__buzy = False
         self.config = config
         self.serialdevice = SerialDevice(config)
         self.debug = {}
+    def init(motor_name: str)
 
-    def move(self, motor_name:str, position: int):
-        pass
-    def goto(self, motor_name:str, position: int):
-        pass
-    def set_position(self, motor_name:str, position: int):
-        pass
-    def goto_home(self, motor_name:str):
-        pass
-    def goto_minus_limit(self, motor_name:str):
-        pass
-    def goto_plus_limit(self, motor_name:str):
-        pass
-    def stop(self, motor_name:str):
+    def goto(self, motor_name: str, position: int):
         pass
 
+    def set_position(self, motor_name: str, position: int):
+        pass
 
+    def get_position(self, motor_name: str, position: int):
+        pass
+
+    def goto_home(self, motor_name: str):
+        pass
+
+    def goto_minus_limit(self, motor_name: str):
+        pass
+
+    def goto_plus_limit(self, motor_name: str):
+        pass
+
+    def stop(self, motor_name: str):
+        pass
 
 
 if __name__ == '__main__':
     from config.configurations import Configuration
+
     a = Configuration()['MCU-2']
     MotorsSystems(a)
